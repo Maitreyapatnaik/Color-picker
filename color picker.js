1243f55175d29c2f843d88032ee4501cc08d16d1
Maitreyapatnaik <113660413+Maitreyapatnaik@users.noreply.github.com>
2024-11-26 11:46:15 +0530

    Add files via upload

---
+++ color picker.js
@@ -0,0 +1,123 @@
+//colors for text
+function textcolor1(){
+    let t= document.getElementById("text");
+    t.style.color="red";
+}
+function textcolor2(){
+    let t= document.getElementById("text");
+    t.style.color="green";
+}
+function textcolor3(){
+    let t= document.getElementById("text");
+    t.style.color="black";
+}
+function textcolor4(){
+    let t= document.getElementById("text");
+    t.style.color="aqua";
+}
+function textcolor5(){
+    let t= document.getElementById("text");
+    t.style.color="sienna";
+}
+function textcolor6(){
+    let t= document.getElementById("text");
+    t.style.color="darkmagenta";
+}
+function textcolor7(){
+    let t= document.getElementById("text");
+    t.style.color="lightcoral";
+}
+function textcolor8(){
+    let t= document.getElementById("text");
+    t.style.color="mediumblue";
+}
+function textcolor9(){
+    let t= document.getElementById("text");
+    t.style.color="yellow";
+}
+function textcolor10(){
+    let t= document.getElementById("text");
+    t.style.color="darkgoldenrod";
+}
+//colors for background
+function bgcolor1(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="red";
+}
+function bgcolor2(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="green";
+}
+function bgcolor3(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="black";
+}
+function bgcolor4(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="aqua";
+}
+function bgcolor5(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="darkgoldenrod";
+}
+function bgcolor6(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="hotpink";
+}
+function bgcolor7(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="mediumblue";
+}
+function bgcolor8(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="lightcoral";
+}
+function bgcolor9(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="darkmagenta";
+}
+function bgcolor10(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="sienna";
+}
+//font styles for text
+function fontstyle1(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="Colonna MT";
+}
+function fontstyle2(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="Times New Roman";
+}
+function fontstyle3(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="SimSun-ExtB";
+}
+function fontstyle4(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="FZShuTi";
+}
+function fontstyle5(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="Microsoft Yi Baiti";
+}
+function fontstyle6(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="PMingLiU-ExtB";
+}
+function fontstyle7(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="MingLiU-ExtB";
+}
+function fontstyle8(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="Gill Sans";
+}
+function fontstyle9(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="Franklin Gothic";
+}
+function fontstyle10(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="Forte";
+}
